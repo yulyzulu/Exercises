c3047e0cc0665e6ca7fe19d332861518174a9cf7
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-07-25 22:05:41 +0000

    Added insert new node function in a single linked list

--- 0x01-insert_in_sorted_linked_list/lists.h
+++ 0x01-insert_in_sorted_linked_list/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -8,11 +11,12 @@
  *
  * Description: singly linked list node structure
  * for Holberton project
- */
+*/
+
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -21,4 +25,4 @@ void free_listint(listint_t *head);
 
 listint_t *insert_node(listint_t **head, int number);
 
-#endif /* LISTS_H */
+#endif
