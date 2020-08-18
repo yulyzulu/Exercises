3a69677224a2bb83d3562699ab70e441e3420c54
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-08-18 03:00:03 +0000

    Added sandpiles exercise

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -7,39 +7,94 @@
 */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, x, y, sum[3][3];
+	int i, j, x, y;
+	int sandpile[3][3] = {
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0}};
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+	}
+	while(sandpile_static(grid1) == 1)
+	{
+		printf("=\n");
+		print_grid(grid1);
+		copy(sandpile, grid1);
+		for (x = 0; x < 3; x++)
+		{
+			for (y = 0; y < 3; y++)
+			{
+				if (sandpile[x][y] > 3)
+				{
+					grid1[x][y] = grid1[x][y] - 4;
+					if (x != 2)
+						grid1[x+1][y] = grid1[x+1][y] + 1;
+					if (x != 0)
+        	                  	      grid1[x-1][y] = grid1[x-1][y] + 1;
+					if (y != 2)
+						grid1[x][y+1] = grid1[x][y+1] + 1;
+					if (y != 0)
+						grid1[x][y-1] = grid1[x][y-1] + 1;
+				}
+			}
+		}
+	}
+}
+/*
+*
+*
+*/
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+/**
+*
+*
+*/
+void copy(int sandpile[3][3], int grid1[3][3])
+{
+	int i, j;
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			if (j)
-				printf(" ");
-			sum[i][j] = grid1[i][j] + grid2[i][j];
-                        printf("%d", sum[i][j]);
+			sandpile[i][j] = grid1[i][j];
 		}
-		printf("\n");
 	}
-	printf("\n");
 
-	for (x = 0; x < 3; x++)
-	{
-		for (y = 0; y < 3; y++)
-		{
-/*			printf("%d", sum[x][y]);*/
+}
+/**
+*
+*
+*/
+int sandpile_static(int sandpile[3][3])
+{
+	int x, y;
 
-                        if (y)
-                                printf(" ");
-			if (sum[x][y] > 3)
-			{
-				sum[x][y] = sum[x][y] - 4;
-				sum[x+1][y] = sum[x+1][y] + 1;
-                                sum[x-1][y] = sum[x-1][y] + 1;
-				sum[x][y+1] = sum[x][y+1] + 1;
-				sum[x][y-1] = sum[x][y-1] + 1;
-			}
-			printf("%d", sum[x][y]);
+        for (x = 0; x < 3; x++)
+        {
+                for (y = 0; y < 3; y++)
+                {
+
+                        if (sandpile[x][y] > 3)
+				return (1);
 		}
-		printf("\n");
 	}
+	return (0);
 }
